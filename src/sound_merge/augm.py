11bf60bb74661320326ea3876d4354e90994e178
XEas <mokeevgleb5@gmail.com>
2024-07-15 14:31:20 -0700

    initial augmentation

--- src/sound_merge/augm.py
+++ src/sound_merge/augm.py
@@ -1,10 +1,26 @@
-from pydub import AudioSegment
-from pydub.playback import play
-from config import *
-from merge import WaveObject
+from pydub import AudioSegment, effects
+import random
 
-music1 = AudioSegment.from_file(str(test_data / "music1.wav"), format="wav") # 44.1 kHz 16 bit
-music2 = AudioSegment.from_file(str(test_data / "music2.wav"), format="wav") # 44.1 kHz 16 bit
-speech1 = AudioSegment.from_file(str(test_data / "voice1.wav"), format="wav") # 44.1 kHz 16 bit
-speech2 = AudioSegment.from_file(str(test_data / "voice2.wav"), format="wav") # 44.1 kHz 16 bit
+random.seed(42)
+
+def random_silence_mask(audio_segment, total_silence_duration):
+    total_silence_duration = min(total_silence_duration, len(audio_segment))
+
+    silence_interval_duration = 1000 
+    num_intervals = total_silence_duration // silence_interval_duration
+
+    start_points = sorted(random.sample(range(0, len(audio_segment) - silence_interval_duration), num_intervals))
+    modified_audio = audio_segment[:]
+
+    for start in start_points:
+        silence_segment = AudioSegment.silent(duration=silence_interval_duration)
+
+        modified_audio = modified_audio[:start] + silence_segment + modified_audio[start + silence_interval_duration:]
+
+    return modified_audio
+
+def fade():
+    pass
+
+
 
---
+++ src/sound_merge/becnhmark.py
@@ -0,0 +1,2 @@
+def mix():
+    pass
