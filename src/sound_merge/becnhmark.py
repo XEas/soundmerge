88098fa4d489702b331f6ba528d6e8f80fc53c80
XEas <mokeevgleb5@gmail.com>
2024-07-16 14:48:51 -0700

    coef adding method

--- src/sound_merge/becnhmark.py
+++ src/sound_merge/becnhmark.py
@@ -1,2 +1,6 @@
-def mix():
-    pass
+"""Two directories of normalized sound files.
+    1. Randomly choose speech
+    2. Randomly choose background
+    3. Randomly choose two coefficients 0-1 for loudness                
+
+"""
