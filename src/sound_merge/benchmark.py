d646ea078d92559c476452aa4e71af8eef81be3a
XEas <mokeevgleb5@gmail.com>
2024-07-23 11:14:26 -0700

    typy annotations

--- src/sound_merge/benchmark.py
+++ src/sound_merge/benchmark.py
@@ -8,10 +8,10 @@ from benchmark_gui import root
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def random_coefficient():
+def random_coefficient() -> float:
     return random.random()
 
-def choose_volume(coefs : list, distr: str):
+def choose_volume(coefs : list, distr: str) -> float:
     if distr == 'uniform':
         return random.choice(coefs)
     elif distr == 'normal':
@@ -19,25 +19,24 @@ def choose_volume(coefs : list, distr: str):
     
     raise ValueError("Distribution type must be 'uniform' or 'normal'") 
 
-def mixture(sc1, sc2, audio_segment1, audio_segment2):
+def mixture(sc1 : float, sc2 : float, audio_segment1 : AudioSegment, audio_segment2 : AudioSegment) -> AudioSegment:
     enh1 = audio_segment1 - calculate_db_loss(sc1)
     enh2 = audio_segment2 - calculate_db_loss(sc2)
     mixed_segment = mix_overlay(enh1, enh2)
 
     return mixed_segment
 
-def choose_audio(path):
-
+def choose_audio(path : Path):
     audio_files = [file for file in path.glob('*.wav') if not file.name.startswith('._')]
     if audio_files:
         return random.choice(audio_files)
     return None
 
-def calculate_db_loss(percent):
+def calculate_db_loss(percent : float):
     return -10 * np.log10(percent)
 
 
-def simple_benchmark(num, dest, dir1, dir2, percentile1, percentile2, distr, progress_bar=None):
+def simple_benchmark(num : int, dest, dir1, dir2, percentile1 : int, percentile2 : int, distr : str, min_duration : float, progress_bar=None) -> None:
     dest, dir1, dir2 = Path(dest), Path(dir1), Path(dir2)
 
     dir1_norm = get_percentile_dBFS(dir1, percentile1)
@@ -83,15 +82,15 @@ def simple_benchmark(num, dest, dir1, dir2, percentile1, percentile2, distr, pro
             root.update_idletasks()
 
 
-def dynamic_select_benchmark(num, dist, dirs, p, progress_bar):
-    dist, dir1, dir2 = Path(dist), Path(dir1), Path(dir2)
-
+def dynamic_select_benchmark(num, dest, dirs, percentiles, progress_bar):
+    dest, dir1, dir2 = Path(dest), Path(dir1), Path(dir2)
+    path_files = [Path(str_path) for str_path in str_paths]
     dir1_norm = get_percentile_dBFS(dir1, percentile1)
     logging.info(f"Dir 2 {percentile1 * 100} percentile: {dir1_norm} dBFS")
 
     dir2_norm = get_percentile_dBFS(dir2, percentile2)
     logging.info(f"Dir 2 {percentile2 * 100} percentile: {dir2_norm} dBFS")
-
+    
     for i in range(num):
         logging.info(f"---------New Audio {i+1}---------")
         audio_1 = choose_audio(dir1)
@@ -121,7 +120,7 @@ def dynamic_select_benchmark(num, dist, dirs, p, progress_bar):
         chosen_volume = choose_volume([segment_1.dBFS, segment_2.dBFS], distr)
         logging.info(f"Finanl volume: {chosen_volume}")
         mixed_segment = mixed_segment.apply_gain(chosen_volume - mixed_segment.dBFS)
-        mixed_segment.export(dist / f"audio{i+1}.wav", format='wav')
+        mixed_segment.export(dest / f"audio{i+1}.wav", format='wav')
         
         if progress_bar:
             progress = ((i + 1) / num) * 100
